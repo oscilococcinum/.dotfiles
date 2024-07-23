9ae59744987883d7b49da76dfd28e2ece7bf9d93
oscilococcinum <bargaw28@gmail.com>
2024-07-23 16:02:52 +0200

    add aliases for ls

--- fish/config.fish
+++ fish/config.fish
@@ -2,8 +2,11 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-alias nswitch="sudo nixos-rebuild switch"
 set fish_greeting
+alias nswitch="sudo nixos-rebuild switch"
 alias nsh="nix-shell --run fish"
 alias cdh="cd ~/"
 alias v="nvim ."
+alias ll="ls -l"
+alias l="ls -la"
+alias nconf="nvim ~/.dotfiles"
