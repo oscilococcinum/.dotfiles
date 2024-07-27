11030b9e98f2e0dde56e74346446d5d6071318fc
oscilococcinum <bargaw28@gmail.com>
2024-07-28 00:43:49 +0200

    add new openfoam pkg to flake

--- flake.nix
+++ flake.nix
@@ -30,14 +30,11 @@
             src = inputs."ondsel-appimage-pre-${system}";
           };
 
-          parmetis-shared = pkgs.callPackage (import ./custompkgs/parmetis-shared) { };
-          scotch-shared = pkgs.callPackage (import ./custompkgs/scotch-shared) { };
-          openfoam-2306 = pkgs.callPackage (import ./custompkgs/openfoam) { parmetis = parmetis-shared; scotch = scotch-shared; };
-          openfoam-2312 = openfoam-2306.override { version = "2312"; hash = "0wgfyz4q7xr0vlv4znfxpxyp8jb53q5pl17k312dyb3x8gkdx2z4"; };
           cfmesh-cfdof = pkgs.callPackage (import ./custompkgs/cfmesh-cfdof) { openfoam = openfoam-2306; };
           hisa = pkgs.callPackage (import ./custompkgs/hisa) { openfoam = openfoam-2306; };
           cfmesh-cfdof-unstable = cfmesh-cfdof.override { version = "unstable"; };
           hisa-unstable = hisa.override { version = "unstable"; };
+          openfoam = pkgs.callPackage (import ./custompkgs/openfoam-com) { };
         };
       }
     )[ "x86_64-linux" "aarch64-linux" ]);
