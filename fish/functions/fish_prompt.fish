5e07d899ddb1dce12d167b4750955388e16644e7
oscilococcinum <bargaw28@gmail.com>
2024-08-03 21:29:13 +0200

    visible prompt when in  nixshell

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -58,4 +58,12 @@ function fish_prompt --description 'Write out the prompt'
     set_color normal
 
     echo -n "$suffix "
+
+    set -l nix_shell_info (
+      if test -n "$IN_NIX_SHELL"
+        echo -n "<nix-shell> "
+      end
+    )
+    echo -n -s "$nix_shell_info"
+    
 end
