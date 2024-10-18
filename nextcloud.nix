cf5a6a6dffa8d47ac646856a3b781509dbf53bd8
oscilococcinum <gawlikbar@gmail.com>
2024-10-18 14:05:45 +0200

    adds ssl-"Let's Encrypt TLS certificates"

--- nextcloud.nix
+++ nextcloud.nix
@@ -4,7 +4,8 @@
   services.nextcloud = {
     enable = true;
     package = pkgs.nextcloud29;
-    hostName = "192.168.1.28";
+    hostName = "nikradap.ddns.net";
+    https = true;
     #home = "/";
     config.adminpassFile = "${pkgs.writeText "adminpass" "test123"}"; #Only for first run. Change pass after run using "nextcloud-occ user:resetpassword root"
     settings.trusted_domains = [ "192.168.1.28" "nikradap.ddns.net" ];
@@ -14,4 +15,17 @@
   };
 
   networking.firewall.allowedTCPPorts = [ 80 443 9980 ];
+  
+  services.nginx.virtualHosts.${config.services.nextcloud.hostName} = {
+    forceSSL = true;
+    enableACME = true;
+  };
+  
+  security.acme = {
+    acceptTerms = true;   
+    certs = { 
+      ${config.services.nextcloud.hostName}.email = "gawlikbar@gmail.com"; 
+    }; 
+  };
+
 }
