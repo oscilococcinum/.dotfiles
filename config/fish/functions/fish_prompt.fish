53ce8ec1a8da8e11cae5008d06d751bab3f49dcd
oscilococcinum <bargaw28@gmail.com>
2024-11-22 21:13:23 +0100

    changes nix shell prompt to shorter version

--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -61,7 +61,7 @@ function fish_prompt --description 'Write out the prompt'
 
     set -l nix_shell_info (
       if test -n "$IN_NIX_SHELL"
-        echo -n "<nix-shell> "
+        echo -n "<nix-sh> "
       end
     )
 
