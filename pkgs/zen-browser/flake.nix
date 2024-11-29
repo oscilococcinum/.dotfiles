c73b951d7619fe3eedfccfb4facfc0cb18928568
oscilococcinum <bargaw28@gmail.com>
2024-11-29 14:42:53 +0100

    updates zen pkg to a.22

--- pkgs/zen-browser/flake.nix
+++ pkgs/zen-browser/flake.nix
@@ -8,16 +8,16 @@
   outputs = { self, nixpkgs }:
     let
       system = "x86_64-linux";
-      version = "1.0.1-a.19";
+      version = "1.0.1-a.22";
       downloadUrl = {
         "specific" = {
-	      url = "https://github.com/zen-browser/desktop/releases/download/${version}/zen.linux-specific.tar.bz2";
-	      sha256 = "sha256:159sx5zqckm25zkw9yi53v8n4ml9y8kndxmy4426rrg16g5a0s4r";
-	    };
-	    "generic" = {
-	      url = "https://github.com/zen-browser/desktop/releases/download/${version}/zen.linux-generic.tar.bz2";
-	      sha256 = "sha256:1n1cq0j8hifvwanqs3wsy5q69w04h397q09adxmdbydm6m8xn5k0";
-	    };
+	  url = "https://github.com/zen-browser/desktop/releases/download/${version}/zen.linux-specific.tar.bz2";
+	  sha256 = "sha256:0anr79rdm62h5y37xa47rrrk32r9gnv04df4z7brc0hp4q83pxvi";
+	};
+	"generic" = {
+	  url = "https://github.com/zen-browser/desktop/releases/download/${version}/zen.linux-generic.tar.bz2";
+	  sha256 = "sha256:065rl1fhg79bkj1qy960qcid7wr7vd7j3wsf7bbr69b4rgmqqv3z";
+	};
       };
 
       pkgs = import nixpkgs {
@@ -28,61 +28,62 @@
         libGL libGLU libevent libffi libjpeg libpng libstartup_notification libvpx libwebp
         stdenv.cc.cc fontconfig libxkbcommon zlib freetype
         gtk3 libxml2 dbus xcb-util-cursor alsa-lib libpulseaudio pango atk cairo gdk-pixbuf glib
-	    udev libva mesa libnotify cups pciutils
-	    ffmpeg libglvnd pipewire
+	udev libva mesa libnotify cups pciutils
+	ffmpeg libglvnd pipewire
       ] ++ (with pkgs.xorg; [
         libxcb libX11 libXcursor libXrandr libXi libXext libXcomposite libXdamage
-	    libXfixes libXScrnSaver
+	libXfixes libXScrnSaver
       ]);
 
       mkZen = { variant }: 
         let
-	      downloadData = downloadUrl."${variant}";
-	    in
-          pkgs.stdenv.mkDerivation {
-              inherit version;
-		      pname = "zen-browser";
-  
-		      src = builtins.fetchTarball {
-		        url = downloadData.url;
-		        sha256 = downloadData.sha256;
-		      };
-		  
-		    desktopSrc = ./.;
-  
-		    phases = [ "installPhase" "fixupPhase" ];
-  
-		    nativeBuildInputs = [ pkgs.makeWrapper pkgs.copyDesktopItems pkgs.wrapGAppsHook ] ;
-  
-		    installPhase = ''
-		      mkdir -p $out/bin && cp -r $src/* $out/bin
-		      install -D $desktopSrc/zen.desktop $out/share/applications/zen.desktop
-		      install -D $src/browser/chrome/icons/default/default128.png $out/share/icons/hicolor/128x128/apps/zen.png
-		    '';
-  
-		    fixupPhase = ''
-		      chmod 755 $out/bin/*
-		      patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" $out/bin/zen
-		      wrapProgram $out/bin/zen --set LD_LIBRARY_PATH "${pkgs.lib.makeLibraryPath runtimeLibs}" \
-                        --set MOZ_LEGACY_PROFILES 1 --set MOZ_ALLOW_DOWNGRADE 1 --set MOZ_APP_LAUNCHER zen --prefix XDG_DATA_DIRS : "$GSETTINGS_SCHEMAS_PATH"
-		      patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" $out/bin/zen-bin
-		      wrapProgram $out/bin/zen-bin --set LD_LIBRARY_PATH "${pkgs.lib.makeLibraryPath runtimeLibs}" \
-                        --set MOZ_LEGACY_PROFILES 1 --set MOZ_ALLOW_DOWNGRADE 1 --set MOZ_APP_LAUNCHER zen --prefix XDG_DATA_DIRS : "$GSETTINGS_SCHEMAS_PATH"
-		      patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" $out/bin/glxtest
-		      wrapProgram $out/bin/glxtest --set LD_LIBRARY_PATH "${pkgs.lib.makeLibraryPath runtimeLibs}"
-		      patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" $out/bin/updater
-		      wrapProgram $out/bin/updater --set LD_LIBRARY_PATH "${pkgs.lib.makeLibraryPath runtimeLibs}"
-		      patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" $out/bin/vaapitest
-		      wrapProgram $out/bin/vaapitest --set LD_LIBRARY_PATH "${pkgs.lib.makeLibraryPath runtimeLibs}"
-		    '';
-  
-            meta.mainProgram = "zen";
+	  downloadData = downloadUrl."${variant}";
+	in
+             pkgs.stdenv.mkDerivation {
+    inherit version;
+		pname = "zen-browser";
+
+		src = builtins.fetchTarball {
+		  url = downloadData.url;
+		  sha256 = downloadData.sha256;
+		};
+		
+		desktopSrc = ./.;
+
+		phases = [ "installPhase" "fixupPhase" ];
+
+		nativeBuildInputs = [ pkgs.makeWrapper pkgs.copyDesktopItems pkgs.wrapGAppsHook ] ;
+
+		installPhase = ''
+		  mkdir -p $out/bin && cp -r $src/* $out/bin
+		  install -D $desktopSrc/zen.desktop $out/share/applications/zen.desktop
+		  install -D $src/browser/chrome/icons/default/default128.png $out/share/icons/hicolor/128x128/apps/zen.png
+		'';
+
+		fixupPhase = ''
+		  chmod 755 $out/bin/*
+		  patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" $out/bin/zen
+		  wrapProgram $out/bin/zen --set LD_LIBRARY_PATH "${pkgs.lib.makeLibraryPath runtimeLibs}" \
+                    --set MOZ_LEGACY_PROFILES 1 --set MOZ_ALLOW_DOWNGRADE 1 --set MOZ_APP_LAUNCHER zen --prefix XDG_DATA_DIRS : "$GSETTINGS_SCHEMAS_PATH"
+		  patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" $out/bin/zen-bin
+		  wrapProgram $out/bin/zen-bin --set LD_LIBRARY_PATH "${pkgs.lib.makeLibraryPath runtimeLibs}" \
+                    --set MOZ_LEGACY_PROFILES 1 --set MOZ_ALLOW_DOWNGRADE 1 --set MOZ_APP_LAUNCHER zen --prefix XDG_DATA_DIRS : "$GSETTINGS_SCHEMAS_PATH"
+		  patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" $out/bin/glxtest
+		  wrapProgram $out/bin/glxtest --set LD_LIBRARY_PATH "${pkgs.lib.makeLibraryPath runtimeLibs}"
+		  patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" $out/bin/updater
+		  wrapProgram $out/bin/updater --set LD_LIBRARY_PATH "${pkgs.lib.makeLibraryPath runtimeLibs}"
+		  patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" $out/bin/vaapitest
+		  wrapProgram $out/bin/vaapitest --set LD_LIBRARY_PATH "${pkgs.lib.makeLibraryPath runtimeLibs}"
+		'';
+
+    meta.mainProgram = "zen";
 	      };
-    in {
+    in
+    {
       packages."${system}" = {
         generic = mkZen { variant = "generic"; };
         specific = mkZen { variant = "specific"; };
-	    default = self.packages."${system}".specific;
+	default = self.packages."${system}".specific;
       };
     };
 }
