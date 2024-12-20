b354db67d6146f1e8dcb05da967df624eeb10f9a
oscilococcinum <gawlikbar@gmail.com>
2024-12-20 19:21:13 +0100

    [CONFIG] sets nvim as default lazygit editor

--- other.nix
+++ other.nix
@@ -1,6 +1,6 @@
 { config, ... }:{
   virtualisation.spiceUSBRedirection.enable = true;
-  environment.variables.EDITOR = "neovim";
+  environment.variables.EDITOR = "nvim";
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
   nixpkgs.config.allowUnfree = true;
   system.stateVersion = "23.11";
