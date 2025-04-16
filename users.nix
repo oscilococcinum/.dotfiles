4c6127bbd3c8261f42b34658ffb8e9dc29f96113
oscilococcinum <bargaw28@gmail.com>
2025-04-16 12:54:06 +0200

    moves all pkgs to global-pkgs.nix

--- users.nix
+++ users.nix
@@ -1,31 +1,8 @@
-{ pkgs, ... }:
-let
-  zen-browser-nix = (builtins.getFlake "github:oscilococcinum/zen-browser-nix").packages.x86_64-linux;
-  freecad-nix = (builtins.getFlake "github:oscilococcinum/freecad-nix").packages.x86_64-linux;
-  nix-alien-pkg = import (builtins.fetchTarball "https://github.com/thiagokokada/nix-alien/tarball/master") { };
-in {
+{ pkgs, ... }:{
   users.users.oscilo = {
     shell = pkgs.fish;
     isNormalUser = true;
     description = "oscilo";
     extraGroups = [ "storage" "networkmanager" "wheel" "video" "dialout" "davfs2" ];
-    packages = (with pkgs; [
-      git
-      lazygit
-      libsForQt5.okular
-      vesktop
-      prusa-slicer
-      p7zip
-      spacenavd
-      paraview
-      mpi
-      elmerfem
-      kdePackages.kdenlive
-      python312
-    ]) ++ [
-      zen-browser-nix.zen-twilight-appimage
-      freecad-nix.freecad-appimage
-      nix-alien-pkg.nix-alien
-    ];
   };
 }
